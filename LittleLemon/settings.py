899011d8c97dd200f8f7db2bc037f274741067bb
Sadat <fsaeed033@gmail.com>
2025-06-29 15:22:42 -0400

    database modified

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -77,10 +77,6 @@ WSGI_APPLICATION = "LittleLemon.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
-    "littlelemon": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "littlelemonrestaurant",
         # ----------------------------------
